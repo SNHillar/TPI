204db8e8bd923db9d373c9ca1f763a7c740f156c
SNHillar <saulhillar@outlook.es>
2025-11-13 12:09:59 -0300

    update credencialAccessDAO methods

--- TPI-Programacion2/src/dao/CredencialAccesoDAO.java
+++ TPI-Programacion2/src/dao/CredencialAccesoDAO.java
@@ -14,13 +14,13 @@ import models.Usuario;
 
 public class CredencialAccesoDAO implements GenericDAO<CredencialAcceso>{
     
-    
-    private static final UsuarioDAO usuarioDAO = new UsuarioDAO();
-    
     private static final String INSERT_SQL = "INSERT INTO credencial_acceso (hash_password, salt, user_id) VALUES (?, ?, ?);"; // los otros atributos, tienen default en la base de datos
     
     private static final String UPDATE_SQL = "UPDATE credencial_acceso SET hash_password = ?, salt = ?, last_change = NOW(), require_reset = FALSE"
             + "WHERE user_id = ?";
+    
+    
+    private static final String DELETE_SQL = "DELET FROM credencial_acceso WHERE id = ?";
     
     private static final String SELECT_BY_ID = "SELECT * FROM credencial_acceso WHERE user_id = ?";
     
@@ -56,24 +56,70 @@ public class CredencialAccesoDAO implements GenericDAO<CredencialAcceso>{
 
     @Override
     public void update(CredencialAcceso entidad) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try(Connection conn = DatabaseConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(UPDATE_SQL)){
+            stmt.setString(1, entidad.getHashPassword());
+            stmt.setString(2, entidad.getSalt());
+            stmt.setInt(3, entidad.getUserId());
+            
+            int rowsAffected = stmt.executeUpdate();
+            
+            if(rowsAffected == 0){
+                throw new SQLException ("Falló la actualizacion de la contrasenia.");
+            }
+        } catch (SQLException e){
+            System.out.println("Error, no se pudo actualizar la contrasenia." + e.getMessage());
+            throw e;
+        }
     }
 
     
     // utilizamos ON DELETE CASCADE en la base de datos, si se borra un usuario, se borra su credencial
     @Override
-    public void delete(int id) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void delete(int id) throws SQLException {
+        // se implementa el metodo para que compile, pero el Service no va a hacer uso porque lo maneja la BD.
+        try(Connection conn = DatabaseConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(DELETE_SQL)){
+            stmt.setInt(1, id);
+            
+            int rowsAffected = stmt.executeUpdate();
+            
+            if(rowsAffected == 0){
+                throw new SQLException ("No se pudo eliminar la credencial con id: " + id);
+            }
+        } catch (SQLException e){
+            System.out.println("Error en credencialAccesoDAO.delete()" + e.getMessage());
+            throw e;
+        }
+    }
+    
+    @Override
+    public CredencialAcceso findById(int id) throws SQLException {
+        // no tiene funcionalidad pero implementamos porque es inofensivo el metodo, solo para cumplir contrato de interfaz
+        try(Connection conn = DatabaseConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(SELECT_BY_ID)){
+            stmt.setInt(1, id);
+            
+            try(ResultSet rs = stmt.executeQuery()){
+                if(rs.next()){
+                    CredencialAcceso credencial = new CredencialAcceso();
+                    credencial.setId(rs.getInt("id"));
+                    credencial.setHashPassword(rs.getString("hash_password"));
+                    credencial.setSalt(rs.getString("salt"));
+                    credencial.setUserId(rs.getInt("user_id"));
+                    return credencial;
+                }
+            }
+        }
+        return null;
     }
 
     @Override
-    public CredencialAcceso findById(int id) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public List<CredencialAcceso> findByAll() throws IllegalArgumentException{
+        // NO queremos listar las contraseñas, no tiene ninguna finalidad, y listar 500.000 hashes pueden generar problemas de rendimiento, etc
+        throw new IllegalArgumentException("No se permite obtener un listado con las contraseñas.");
     }
 
-    @Override
-    public List<CredencialAcceso> findByAll() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    
+    
+    public CredencialAcceso findByUsername(String username){
+        return null;
     }
-
 }
